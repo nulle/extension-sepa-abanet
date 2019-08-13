15f5d6be175ab323904e3d01c9670329c0a75ad0
Inga Brūnava <inga.brunava@gmail.com>
2019-08-13 19:17:00 +0200

    Fixed id for the new Abanet layout

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 
 function fillOrder(order) {
 	var changeEvent,
-		idPrefix = '#ctl00_ctl00_ctl00_cphbody_cphbody_cphbody_';
+		idPrefix = '#ctl00_ctl00_ctl00_cphbody_cphbody_cphbody_tabcontrol_';
 
 	$(idPrefix + 'txtTrkDobro').val(order.iban);
 	$(idPrefix + 'txtZnesek').val(order.amount.replace('.', ','));
@@ -12,7 +12,7 @@ function fillOrder(order) {
 	$(idPrefix + 'txtReferencaDobro1').val(order.reference.substr(2,2));
 	$(idPrefix + 'txtReferencaDobro2').val(order.reference.substr(4));
 
-	//triggers fillig bic and address fields from iban
+	//triggers filling bic and address fields from iban
 	changeEvent = document.createEvent("HTMLEvents");
 	changeEvent.initEvent('change', true, true);
 	$(idPrefix + 'txtTrkDobro')[0].dispatchEvent(changeEvent);
